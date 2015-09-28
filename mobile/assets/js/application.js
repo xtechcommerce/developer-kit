24a5c311027788042e548c08697bc7b5665a0a79
Ricardo Oliveira <ron2302@gmail.com>
2015-09-28 01:39:32 -0300

    Template Mobile update

--- mobile/assets/js/application.js
+++ mobile/assets/js/application.js
@@ -1,224 +1,44 @@
+/*!
+ * Bootstrap v3.2.0 (http://getbootstrap.com)
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+if("undefined"==typeof jQuery)throw new Error("Bootstrap's JavaScript requires jQuery");+function(t){"use strict";function e(e,o){return this.each(function(){var s=t(this),n=s.data("bs.modal"),a=t.extend({},i.DEFAULTS,s.data(),"object"==typeof e&&e);n||s.data("bs.modal",n=new i(this,a)),"string"==typeof e?n[e](o):a.show&&n.show(o)})}var i=function(e,i){this.options=i,this.$body=t(document.body),this.$element=t(e),this.$backdrop=this.isShown=null,this.scrollbarWidth=0,this.options.remote&&this.$element.find(".modal-content").load(this.options.remote,t.proxy(function(){this.$element.trigger("loaded.bs.modal")},this))};i.VERSION="3.2.0",i.DEFAULTS={backdrop:!0,keyboard:!0,show:!0},i.prototype.toggle=function(t){return this.isShown?this.hide():this.show(t)},i.prototype.show=function(e){var i=this,o=t.Event("show.bs.modal",{relatedTarget:e});this.$element.trigger(o),this.isShown||o.isDefaultPrevented()||(this.isShown=!0,this.checkScrollbar(),this.$body.addClass("modal-open"),this.setScrollbar(),this.escape(),this.$element.on("click.dismiss.bs.modal",'[data-dismiss="modal"]',t.proxy(this.hide,this)),this.backdrop(function(){var o=t.support.transition&&i.$element.hasClass("fade");i.$element.parent().length||i.$element.appendTo(i.$body),i.$element.show().scrollTop(0),o&&i.$element[0].offsetWidth,i.$element.addClass("in").attr("aria-hidden",!1),i.enforceFocus();var s=t.Event("shown.bs.modal",{relatedTarget:e});o?i.$element.find(".modal-dialog").one("bsTransitionEnd",function(){i.$element.trigger("focus").trigger(s)}).emulateTransitionEnd(300):i.$element.trigger("focus").trigger(s)}))},i.prototype.hide=function(e){e&&e.preventDefault(),e=t.Event("hide.bs.modal"),this.$element.trigger(e),this.isShown&&!e.isDefaultPrevented()&&(this.isShown=!1,this.$body.removeClass("modal-open"),this.resetScrollbar(),this.escape(),t(document).off("focusin.bs.modal"),this.$element.removeClass("in").attr("aria-hidden",!0).off("click.dismiss.bs.modal"),t.support.transition&&this.$element.hasClass("fade")?this.$element.one("bsTransitionEnd",t.proxy(this.hideModal,this)).emulateTransitionEnd(300):this.hideModal())},i.prototype.enforceFocus=function(){t(document).off("focusin.bs.modal").on("focusin.bs.modal",t.proxy(function(t){this.$element[0]===t.target||this.$element.has(t.target).length||this.$element.trigger("focus")},this))},i.prototype.escape=function(){this.isShown&&this.options.keyboard?this.$element.on("keyup.dismiss.bs.modal",t.proxy(function(t){27==t.which&&this.hide()},this)):this.isShown||this.$element.off("keyup.dismiss.bs.modal")},i.prototype.hideModal=function(){var t=this;this.$element.hide(),this.backdrop(function(){t.$element.trigger("hidden.bs.modal")})},i.prototype.removeBackdrop=function(){this.$backdrop&&this.$backdrop.remove(),this.$backdrop=null},i.prototype.backdrop=function(e){var i=this,o=this.$element.hasClass("fade")?"fade":"";if(this.isShown&&this.options.backdrop){var s=t.support.transition&&o;if(this.$backdrop=t('<div class="modal-backdrop '+o+'" />').appendTo(this.$body),this.$element.on("click.dismiss.bs.modal",t.proxy(function(t){t.target===t.currentTarget&&("static"==this.options.backdrop?this.$element[0].focus.call(this.$element[0]):this.hide.call(this))},this)),s&&this.$backdrop[0].offsetWidth,this.$backdrop.addClass("in"),!e)return;s?this.$backdrop.one("bsTransitionEnd",e).emulateTransitionEnd(150):e()}else if(!this.isShown&&this.$backdrop){this.$backdrop.removeClass("in");var n=function(){i.removeBackdrop(),e&&e()};t.support.transition&&this.$element.hasClass("fade")?this.$backdrop.one("bsTransitionEnd",n).emulateTransitionEnd(150):n()}else e&&e()},i.prototype.checkScrollbar=function(){document.body.clientWidth>=window.innerWidth||(this.scrollbarWidth=this.scrollbarWidth||this.measureScrollbar())},i.prototype.setScrollbar=function(){var t=parseInt(this.$body.css("padding-right")||0,10);this.scrollbarWidth&&this.$body.css("padding-right",t+this.scrollbarWidth)},i.prototype.resetScrollbar=function(){this.$body.css("padding-right","")},i.prototype.measureScrollbar=function(){var t=document.createElement("div");t.className="modal-scrollbar-measure",this.$body.append(t);var e=t.offsetWidth-t.clientWidth;return this.$body[0].removeChild(t),e};var o=t.fn.modal;t.fn.modal=e,t.fn.modal.Constructor=i,t.fn.modal.noConflict=function(){return t.fn.modal=o,this},t(document).on("click.bs.modal.data-api",'[data-toggle="modal"]',function(i){var o=t(this),s=o.attr("href"),n=t(o.attr("data-target")||s&&s.replace(/.*(?=#[^\s]+$)/,"")),a=n.data("bs.modal")?"toggle":t.extend({remote:!/#/.test(s)&&s},n.data(),o.data());o.is("a")&&i.preventDefault(),n.one("show.bs.modal",function(t){t.isDefaultPrevented()||n.one("hidden.bs.modal",function(){o.is(":visible")&&o.trigger("focus")})}),e.call(n,a,this)})}(jQuery),+function(t){"use strict";function e(){var t=document.createElement("bootstrap"),e={WebkitTransition:"webkitTransitionEnd",MozTransition:"transitionend",OTransition:"oTransitionEnd otransitionend",transition:"transitionend"};for(var i in e)if(void 0!==t.style[i])return{end:e[i]};return!1}t.fn.emulateTransitionEnd=function(e){var i=!1,o=this;t(this).one("bsTransitionEnd",function(){i=!0});var s=function(){i||t(o).trigger(t.support.transition.end)};return setTimeout(s,e),this},t(function(){t.support.transition=e(),t.support.transition&&(t.event.special.bsTransitionEnd={bindType:t.support.transition.end,delegateType:t.support.transition.end,handle:function(e){return t(e.target).is(this)?e.handleObj.handler.apply(this,arguments):void 0}})})}(jQuery);
+
+
 $.fn.extend({
-
-	mask_brazilian_phone: function(){
-
-		$(this[0]).focusout(function(){
-
-			var phone, element;
-			element = $(this);
-			element.unmask();
-			phone = element.val().replace(/\D/g, '');
-			if(phone.length > 10){
-
-				element.mask("(99) 99999-999?9");
-
-			} else{
-
-				element.mask("(99) 9999-9999?9");
-
-			}
-
-		}).trigger('focusout');
-
-	}
-
+    mask_brazilian_phone: function() {
+        $(this).focusout(function(){
+        var phone, element;
+        element = $(this);
+        element.unmask();
+        phone = element.val().replace(/\D/g, '');
+        if(phone.length > 10) {
+            element.mask("(99) 99999-999?9");
+        } else {
+            element.mask("(99) 9999-9999?9");
+        }
+        }).trigger('focusout');
+    },
+    delayKeyup: function(callback, ms) {
+        var timer = 0;
+        var el = $(this);
+        $(this).keyup(function(){
+        clearTimeout (timer);
+        timer = setTimeout(function(){
+            callback(el);
+            }, ms);
+        });
+        return $(this);
+    }
 });
-
 
 $(document).ready(function(){
 
-	search_current_selection = 0;
-	search_current_url = '';
-
-	$('#search-engine').focus(function(){
-
-		if($('#search-results-in a').size() > 1){
-
-			search_show();
-
-		}
-
-	});
-
-	$('#top-search input').on('keyup', function(e){
-
-		var key = e.keyCode;
-		
-		//Navegar pelo direcional do teclado
-		if(key == 38 || key == 40 || key == 13){
-
-			if($("#search-results a.up").size() == 0) search_current_selection = -1;
-			switch(e.keyCode){
-
-				// Para cima
-				case 38:
-					if(search_current_selection != -1 && search_current_selection != 0) search_current_selection--;
-					break;
-
-				// Para baixo
-				case 40:
-					if(search_current_selection != $("#search-results a").size()-1) search_current_selection++;
-					break;
-
-				// Enter
-				case 13:
-					if(search_current_url == '') $('#form-search').submit();
-					if(search_current_url != '') document.location = search_current_url;
-					break;
-
-			}
-			
-			search_set_selected(search_current_selection);
-
-		} else if(!((key >= 16 && key <= 20) || (key >= 33 && key <= 37) || key == 39 || key == 45 || (key >= 112 && key <= 145))){
-
-			run_search_query();
-
-		}
-
+	$('.tag-filter').change(function(e){
+		if ($(this).val() != '') {
+			window.location.href = $(this).val();
+		};
 	});
 
 });
-
-search_show = function(){
-
-	$('#search-results').show();
-
-}
-
-search_close = function(){
-
-	$("body").click(function(event){
-
-		var targ = $(event.target);
-		if(!targ.is('#search-results') && !targ.is('#top-search input')){
-
-			$('#search-results').hide();
-			$('#search-loader').hide();
-
-		}
-
-	});
-
-}
-
-search_mouse_navigation = function(){
-
-	$("#search-results a").hover(function(){
-
-		if($(this).attr('id') != ''){
-
-			search_current_selection = $(this).attr('id').substr(14);
-			search_set_selected(search_current_selection);
-
-		}
-
-	}, function(){
-
-		$("#search-results a").removeClass("up");
-		search_current_url = '';
-
-	});
-
-}
-
-search_set_selected = function(cur_selection){
-
-	$("#search-results a").removeClass("up");
-	$("#search-results a").eq(cur_selection).addClass("up");
-	search_current_url = $("#search-results a").eq(cur_selection).attr("href");
-
-}
-
-run_search_query = function(){
-
-	$('#search-loader').show();
-
-	var baseurl = $('meta[name=baseurl]').attr("content");
-	$.post(baseurl + "index.php/search/find/", { name: $('#search-engine').val(), limit: 10 },
-		function(data){
-			
-			$('#search-results-in').html('');
-			var count = 'Nenhum produto foi encontrado.';
-
-			var num = 0;
-			$.each(data, function(index, product){
-
-				if(index == 'count'){
-
-					if(product == 50){
-
-						count = 'Foram encontrados mais de '+product+' produtos.';
-
-					} else{
-
-						count = 'Foram encontrados '+product+' produtos.';
-
-					}
-
-				} else{
-
-					$('#search-results').show();
-					if($('.search-result-'+index).length == 0){
-
-						var saleprice = '';
-						var preco = '';
-						var modelo = '';
-
-						$.each(product.categories, function(){
-
-							modelo = this.name;
-
-						});
-
-						if(product.saleprice == '0.00'){
-
-							product_price = product.price.replace(".", ",");
-
-							preco = '<span class="price">R$ '+product_price+'</span>';
-
-						} else{
-
-							product_saleprice = product.saleprice.replace(".", ",");
-							product_price = product.price.replace(".", ",");
-
-							preco = '<span class="price"><span class="saleprice">R$ '+product_price+'</span> R$ '+product_saleprice+'</span>';
-
-						}
-
-						$('#search-results-in').append('<a href="' + baseurl + product.slug + '" id="search-result-' + num + '" class="search-result-' + index +'">' +
-															'<div class="line">' +
-																'<div class="image text-center">' +
-																	product.image +
-																'</div>' +
-																'<div class="desc">' +
-																	'<span class="name">' +
-																		product.name +
-																	'</span>' +
-																	'<span class="category">'
-																		+ modelo +
-																	'</span> ' +
-																	preco +
-																'</div>' +
-																'<div class="clearfix"></div>' +
-															'</div>' +
-														'</a>');
-
-						num++;
-
-					}
-
-				}
-				
-			});
-
-			$('#search-results-in').append('<div class="last text-center"><a href="#" onclick="$(\'#form-search\').submit();"><strong>Listar resultados</strong><br />'+count+'</a></div>');
-			$('#search-loader').hide();
-
-			search_show();
-			search_mouse_navigation();
-			search_close();
-
-	}, 'json');
-
-}
