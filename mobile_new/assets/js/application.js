70e42f1aebee248a0c817e0a5091cab0d4d8ab8c
Ricardo Oliveira <ron2302@gmail.com>
2016-11-28 18:09:55 -0200

    New mobile template

---
+++ mobile_new/assets/js/application.js
@@ -0,0 +1,167 @@
+if("undefined"==typeof jQuery)throw new Error("Bootstrap's JavaScript requires jQuery");+function(t){"use strict";function e(e,o){return this.each(function(){var s=t(this),n=s.data("bs.modal"),a=t.extend({},i.DEFAULTS,s.data(),"object"==typeof e&&e);n||s.data("bs.modal",n=new i(this,a)),"string"==typeof e?n[e](o):a.show&&n.show(o)})}var i=function(e,i){this.options=i,this.$body=t(document.body),this.$element=t(e),this.$backdrop=this.isShown=null,this.scrollbarWidth=0,this.options.remote&&this.$element.find(".modal-content").load(this.options.remote,t.proxy(function(){this.$element.trigger("loaded.bs.modal")},this))};i.VERSION="3.2.0",i.DEFAULTS={backdrop:!0,keyboard:!0,show:!0},i.prototype.toggle=function(t){return this.isShown?this.hide():this.show(t)},i.prototype.show=function(e){var i=this,o=t.Event("show.bs.modal",{relatedTarget:e});this.$element.trigger(o),this.isShown||o.isDefaultPrevented()||(this.isShown=!0,this.checkScrollbar(),this.$body.addClass("modal-open"),this.setScrollbar(),this.escape(),this.$element.on("click.dismiss.bs.modal",'[data-dismiss="modal"]',t.proxy(this.hide,this)),this.backdrop(function(){var o=t.support.transition&&i.$element.hasClass("fade");i.$element.parent().length||i.$element.appendTo(i.$body),i.$element.show().scrollTop(0),o&&i.$element[0].offsetWidth,i.$element.addClass("in").attr("aria-hidden",!1),i.enforceFocus();var s=t.Event("shown.bs.modal",{relatedTarget:e});o?i.$element.find(".modal-dialog").one("bsTransitionEnd",function(){i.$element.trigger("focus").trigger(s)}).emulateTransitionEnd(300):i.$element.trigger("focus").trigger(s)}))},i.prototype.hide=function(e){e&&e.preventDefault(),e=t.Event("hide.bs.modal"),this.$element.trigger(e),this.isShown&&!e.isDefaultPrevented()&&(this.isShown=!1,this.$body.removeClass("modal-open"),this.resetScrollbar(),this.escape(),t(document).off("focusin.bs.modal"),this.$element.removeClass("in").attr("aria-hidden",!0).off("click.dismiss.bs.modal"),t.support.transition&&this.$element.hasClass("fade")?this.$element.one("bsTransitionEnd",t.proxy(this.hideModal,this)).emulateTransitionEnd(300):this.hideModal())},i.prototype.enforceFocus=function(){t(document).off("focusin.bs.modal").on("focusin.bs.modal",t.proxy(function(t){this.$element[0]===t.target||this.$element.has(t.target).length||this.$element.trigger("focus")},this))},i.prototype.escape=function(){this.isShown&&this.options.keyboard?this.$element.on("keyup.dismiss.bs.modal",t.proxy(function(t){27==t.which&&this.hide()},this)):this.isShown||this.$element.off("keyup.dismiss.bs.modal")},i.prototype.hideModal=function(){var t=this;this.$element.hide(),this.backdrop(function(){t.$element.trigger("hidden.bs.modal")})},i.prototype.removeBackdrop=function(){this.$backdrop&&this.$backdrop.remove(),this.$backdrop=null},i.prototype.backdrop=function(e){var i=this,o=this.$element.hasClass("fade")?"fade":"";if(this.isShown&&this.options.backdrop){var s=t.support.transition&&o;if(this.$backdrop=t('<div class="modal-backdrop '+o+'" />').appendTo(this.$body),this.$element.on("click.dismiss.bs.modal",t.proxy(function(t){t.target===t.currentTarget&&("static"==this.options.backdrop?this.$element[0].focus.call(this.$element[0]):this.hide.call(this))},this)),s&&this.$backdrop[0].offsetWidth,this.$backdrop.addClass("in"),!e)return;s?this.$backdrop.one("bsTransitionEnd",e).emulateTransitionEnd(150):e()}else if(!this.isShown&&this.$backdrop){this.$backdrop.removeClass("in");var n=function(){i.removeBackdrop(),e&&e()};t.support.transition&&this.$element.hasClass("fade")?this.$backdrop.one("bsTransitionEnd",n).emulateTransitionEnd(150):n()}else e&&e()},i.prototype.checkScrollbar=function(){document.body.clientWidth>=window.innerWidth||(this.scrollbarWidth=this.scrollbarWidth||this.measureScrollbar())},i.prototype.setScrollbar=function(){var t=parseInt(this.$body.css("padding-right")||0,10);this.scrollbarWidth&&this.$body.css("padding-right",t+this.scrollbarWidth)},i.prototype.resetScrollbar=function(){this.$body.css("padding-right","")},i.prototype.measureScrollbar=function(){var t=document.createElement("div");t.className="modal-scrollbar-measure",this.$body.append(t);var e=t.offsetWidth-t.clientWidth;return this.$body[0].removeChild(t),e};var o=t.fn.modal;t.fn.modal=e,t.fn.modal.Constructor=i,t.fn.modal.noConflict=function(){return t.fn.modal=o,this},t(document).on("click.bs.modal.data-api",'[data-toggle="modal"]',function(i){var o=t(this),s=o.attr("href"),n=t(o.attr("data-target")||s&&s.replace(/.*(?=#[^\s]+$)/,"")),a=n.data("bs.modal")?"toggle":t.extend({remote:!/#/.test(s)&&s},n.data(),o.data());o.is("a")&&i.preventDefault(),n.one("show.bs.modal",function(t){t.isDefaultPrevented()||n.one("hidden.bs.modal",function(){o.is(":visible")&&o.trigger("focus")})}),e.call(n,a,this)})}(jQuery),+function(t){"use strict";function e(){var t=document.createElement("bootstrap"),e={WebkitTransition:"webkitTransitionEnd",MozTransition:"transitionend",OTransition:"oTransitionEnd otransitionend",transition:"transitionend"};for(var i in e)if(void 0!==t.style[i])return{end:e[i]};return!1}t.fn.emulateTransitionEnd=function(e){var i=!1,o=this;t(this).one("bsTransitionEnd",function(){i=!0});var s=function(){i||t(o).trigger(t.support.transition.end)};return setTimeout(s,e),this},t(function(){t.support.transition=e(),t.support.transition&&(t.event.special.bsTransitionEnd={bindType:t.support.transition.end,delegateType:t.support.transition.end,handle:function(e){return t(e.target).is(this)?e.handleObj.handler.apply(this,arguments):void 0}})})}(jQuery);
+
+// Add Brazilian Phone Supporte
+$.fn.extend({
+	mask_brazilian_phone: function(){
+		$(this[0]).focusout(function(){
+			var phone, element;
+			element = $(this);
+			element.unmask();
+			phone = element.val().replace(/\D/g, '');
+			if(phone.length > 10){
+				element.mask("(99) 99999-999?9");
+			} else{
+				element.mask("(99) 9999-9999?9");
+			}
+		}).trigger('focusout');
+	},
+	delayKeyup: function(callback, ms) {
+	  var timer = 0;
+	  var el = $(this);
+
+	  $(this).keyup(function(){
+	  	clearTimeout (timer);
+	  	timer = setTimeout(function(){
+			callback(el);
+      	}, ms);
+	  });
+
+	  return $(this);
+	}
+
+});
+// Resize Function
+$(window).resize(function(){
+    if($('.modal-content').length > 0){
+        $('.modal-dialog').animate({'opacity':'1'});
+
+        popupRes($('#popup .modal-content'));
+        popupRes($('#exit_popup .modal-content'));
+    }
+});
+
+// Ready Function
+$(document).ready(function(){
+	$('header nav').meanmenu();
+
+    $('.formAddCart').on('submit', function(e){
+        e.preventDefault();
+        var serialized = $(this).serialize();
+        $.post(site_url.base+"/cart/add_to_cart", serialized, function(data){
+            var total = "R$ " + data["total"].toFixed(2).replace('.',',');
+            $('#view_cartTotalItens').text(data["total_items"]);
+            $('#view_cartTotalValue').text(total);
+            $('#ProdModal .mod_prodaction .prodprice').html(total);
+            $('#ProdModal').modal();
+        }, 'json');
+
+        return false;
+    });
+
+    // Positioning Popup
+    setTimeout(function(){
+        if($('.modal-dialog').length > 0){
+            $('.modal-dialog').animate({'opacity':'1'});
+            popupRes($('#popup  .modal-content'));
+            popupRes($('#exit_popup  .modal-content'));
+        }
+    }, 200);
+
+    setTimeout(function(){
+        $('#flash .alert').animate({'top':'5px', 'opacity':1}, 500);
+    }, 200);
+
+    $('.close-alert').on('click', function(){
+       $(this).closest('.alert').slideUp();
+    });
+
+	// Link na tab do My Account
+	tabs_hash();
+	$(window).on('hashchange', tabs_hash);
+
+	$('.imgLiquidFill').imgLiquid({
+		fill: false,
+		horizontalAlign: "center",
+		verticalAlign: "center"
+	});
+});
+
+popupRes = function(el){
+    var w_h = $(window).height();
+    var p_h = el.outerHeight();
+    el.animate({'top': (w_h - p_h)/2 + 'px'});
+}
+
+getCookie = function(c_name){
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" cookie=" + c_name);
+    if (c_start == -1) {
+        c_start = c_value.indexOf("cookie=" + c_name);
+    };
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
+
+del_cookie = function(name){
+    document.cookie = 'cookie=' + name + '; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+del_real_cookie = function(cookie,name){
+    document.cookie = cookie + '=' + name + '; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+tabs_hash = function(){
+	if(window.location.hash){
+		var url = window.location.hash;
+		$('#mytabs a[href="'+url+'"]').tab('show')
+	};
+};
+
+function isEmail(email){
+    er = /^[a-zA-Z0-9][a-zA-Z0-9\._-]+@([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2}/;
+    if( !er.exec(email) ){
+      return false;
+    }else{ return true; }
+}
+
+function checkCartMail() {
+    var mail=getRealCookie("cartmail");
+    if (mail!="") {
+        return mail;
+    }
+}
+
+function getRealCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+var cusid_ele = document.getElementsByClassName('product-description');
+var maior = 0;
+
+for (var i = 0; i < cusid_ele.length; ++i) {
+	var item = cusid_ele[i];
+	if (item.offsetHeight > maior){
+		maior = item.offsetHeight;
+	}
+}
+
+for (var i = 0; i < cusid_ele.length; ++i) {
+	var item = cusid_ele[i];
+	item.style.height  = maior + 'px';
+}
---
+++ mobile_new/assets/js/cpf.js
@@ -0,0 +1,141 @@
+/*!
+*	Gerador e Validador de CPF v2.0.1
+*	https://github.com/tiagoporto/gerador-validador-cpf
+*	Copyright (c) 2014-2016 Tiago Porto (http://www.tiagoporto.com)
+*	Released under the MIT license
+*/
+
+/**
+ * CPF Class
+ *
+ * gera function
+ * @param  {string} formatacao Opção para fazer a formatação
+ * @return {string}            CPF válido e formatado
+ *
+ * valida function
+ * @param  {string} valor      O valor para validação
+ * @return {boolean}           True para cpf válido - False para cpf inválido.
+ *
+ * formata function
+ * @param  {string} valor      O valor para formatação
+ * @param  {string} formatacao Opção para fazer a formatação
+ *
+ * @return {string}            O CPF formatado ou mensagem com erro da formatação.
+ */
+
+/* exported CPF */
+function CPF(){
+	'use strict';
+
+	function calculoVerificador1(noveDigitos){
+		var soma = null;
+
+		for (var j = 0; j < 9; ++j) {
+			soma += noveDigitos.toString().charAt(j)*(10-j);
+		}
+
+		var somaFinalVerificador1 = soma % 11,
+			verificador1 = 11 - somaFinalVerificador1;
+
+		if (somaFinalVerificador1 < 2) {
+			verificador1 = 0;
+		}
+
+		return verificador1;
+	}
+
+	function calculoVerificador2(cpfComVerificador1){
+		var soma = null;
+
+		for (var k = 0; k < 10; ++k){
+			soma +=  cpfComVerificador1.toString().charAt(k)*(11-k);
+		}
+
+		var somaFinalVerificador2 = soma % 11,
+			verificador2 = 11 - somaFinalVerificador2;
+
+		if (somaFinalVerificador2 < 2) {
+			verificador2 = 0;
+		}
+
+		return verificador2;
+	}
+
+	function limpa(valor){
+		var digitos = valor.replace(/\.|\-|\s/g,'');
+
+		return digitos;
+	}
+
+	function formataCPF(value, formatacao){
+		var sepDigitos = '.',
+			sepVerificador = '-';
+
+		if (formatacao === 'digitos') {
+			sepDigitos = '';
+			sepVerificador = '';
+		}else if (formatacao === 'verificador') {
+			sepDigitos = '';
+			sepVerificador = '-';
+		}
+
+		if (! /^[0-9]+$/.test(value)) {
+			return 'O valor informado contém caracteres inválidos.';
+		}
+
+		if (value.length >= 11 ) {
+			return 'O valor informado contém erro. Está passando dígitos.';
+		}else if(value.length <= 11){
+			return 'O valor informado contém erro. Está faltando dígitos.';
+
+		}
+
+		else{
+			return value.slice(0, 3) + sepDigitos + value.slice(3, 6) + sepDigitos +  value.slice(6, 9) + sepVerificador +  value.slice(9, 11);
+		}
+	}
+
+	this.gera = function (formatacao){
+		var noveDigitos = '';
+
+		//Gerando os 9 primeiros digitos do CPF
+		for (var i = 0; i < 9; ++i) {
+			noveDigitos += Math.floor(Math.random() * 9) + '';
+		}
+
+		var verificador1 = calculoVerificador1(noveDigitos);
+
+		var getCPF = noveDigitos + verificador1 + calculoVerificador2(noveDigitos + verificador1);
+
+		return formataCPF(getCPF, formatacao);
+	};
+
+	this.valida = function (valor){
+		var clearCPF = limpa(valor),
+			noveDigitos = clearCPF.substring(0,9),
+			verificadores = clearCPF.substring(9,11);
+
+		//Verificando se todos os digitos são iguais
+		for (var i = 0; i < 10; i++){
+			if('' + noveDigitos + verificadores === Array(12).join( i ) ){
+				return false;
+			}
+		}
+
+		var verificador1 = calculoVerificador1(noveDigitos);
+
+		var verificador2 = calculoVerificador2(noveDigitos + '' + verificador1);
+
+		if (verificadores.toString() === verificador1.toString() + verificador2.toString()){
+			return true;
+		}else{
+			return false;
+		}
+	};
+
+	this.formata = function (valor, formatacao){
+		var getCPF = limpa(valor);
+
+		return formataCPF(getCPF, formatacao);
+	};
+}
